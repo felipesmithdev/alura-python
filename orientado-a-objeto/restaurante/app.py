2bfcd826fbb7fd2fb9b464b114f56e5f46c4646e
felipesmithdev <felipedosantos2004@gmail.com>
2026-02-14 22:13:36 -0300

    Adicionando projetos sobre requisições usando FASTAPI

--- orientado-a-objeto/restaurante/app.py
+++ orientado-a-objeto/restaurante/app.py
@@ -1,17 +1,31 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_pizza = Restaurante('Praca', 'Gourmet')
 restaurante_praca = Restaurante('Pizza Express', 'Italiana')
 restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
 
-restaurante_mexicano.alternar_estado()
-restaurante_mexicano.receber_avaliacao('Felipe', 9)
-restaurante_mexicano.receber_avaliacao('Maria', 8)
-restaurante_mexicano.receber_avaliacao('Julia', 2)
+bebida_suco = Bebida('Suco de Melancia', 5.00, 'Grande')
+prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
+
+bebida_suco.aplicar_desconto()
+prato_paozinho.aplicar_desconto()
+
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+
+
+
+
+# restaurante_mexicano.alternar_estado()
+# restaurante_mexicano.receber_avaliacao('Felipe', 9)
+# restaurante_mexicano.receber_avaliacao('Maria', 8)
+# restaurante_mexicano.receber_avaliacao('Julia', 2)
 
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 
 if __name__ == '__main__':
--- orientado-a-objeto/restaurante/modelos/restaurante.py
+++ orientado-a-objeto/restaurante/modelos/restaurante.py
@@ -1,5 +1,5 @@
 from modelos.avaliacao import Avaliacao
-
+from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Restaurante:
     restaurantes = []
@@ -9,6 +9,7 @@ class Restaurante:
         self._categoria = categoria.upper()
         self._ativo = False
         self._avaliacao = []
+        self._cardapio = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -31,6 +32,29 @@ class Restaurante:
         if 0 < nota <= 5:
             avaliacao = Avaliacao(cliente, nota)
             self._avaliacao.append(avaliacao)
+
+    # def adicionar_bebida_no_cardapio(self, Bebida):
+    #     self._cardapio.append(Bebida)
+
+    # def adicionar_prato_no_caradapio(self, Prato):
+    #     self._cardapio.append(Prato)
+
+    def adicionar_no_cardapio(self, item):
+        if isinstance(item, ItemCardapio):
+            self._cardapio.append(item)
+
+    @property
+    def exibir_cardapio(self):
+        print(f'Cardapio do restaurante: {self._nome}\n')
+        for i, item in enumerate(self._cardapio, start=1):
+            mensagem = f'{i}. Nome: {item._nome} | Preço: R${item._preco}'
+            if hasattr(item, 'descricao'):
+                mensagem += f' | {item.descricao}'
+            else:
+                mensagem += f' | {item._tamanho}'
+            print(mensagem)
+
+
     
 
     @property
